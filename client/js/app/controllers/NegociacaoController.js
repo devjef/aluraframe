8ebc11f463ba5465bc65d5d2600a0b3af5a7719c
Jeferson <36243087+devjef@users.noreply.github.com>
2020-06-15 14:18:30 -0300

    Arquivos finais do projeto

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,18 +2,31 @@ class NegociacaoController {
 
     constructor() {
         let $ = document.querySelector.bind(document);
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
         this._listaNegociacoes = new ListaNegociacoes();
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#alertaMensagem'));
+        this._mensagemView.update(this._mensagem);
     };
 
     adiciona(e) {
         e.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao())
-        this._limpaInputs();
 
-        console.log(this._listaNegociacoes.negociacoes)
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+
+        this._limpaInputs();
     };
 
     _criaNegociacao() {
---
+++ client/js/app/views/NegociacoesView.js
@@ -0,0 +1,39 @@
+class NegociacoesView extends View {
+
+    constructor(elemento) {
+       super(elemento);
+    }
+
+    template(model) {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            <tbody id="tbody">
+            ${model.negociacoes.map(n => `
+                    <tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>    
+                `).join('')}
+            </tbody>
+            <tfoot>
+                <td colspan="3"></td>
+                <td><strong>Total:</strong> ${
+                    model.negociacoes.reduce((total, n) => total += n.volume, 0.0)
+                }</td>
+            </tfoot>
+        </table>
+        `
+    };
+
+}
+
