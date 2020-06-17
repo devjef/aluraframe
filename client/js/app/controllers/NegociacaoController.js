0a38995729ee3977bf61c61d3427483b4a63fe21
Jeferson <36243087+devjef@users.noreply.github.com>
2020-06-17 11:49:04 -0300

    Aplicando padrão Observer no projeto
    - Atualização da view assim que a lista for alterada ou apagada
    - Exercicios sobre Arrow Function e seu escopo léxico e API Reflection

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,10 +7,9 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model))
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#alertaMensagem'));
@@ -30,7 +29,7 @@ class NegociacaoController {
     };
 
     _criaNegociacao() {
-        return  new Negociacao(
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
@@ -39,10 +38,17 @@ class NegociacaoController {
 
     _limpaInputs() {
         this._inputData.value = '',
-        this._inputQuantidade.value = 1,
-        this._inputValor.value = 0.0
+            this._inputQuantidade.value = 1,
+            this._inputValor.value = 0.0
 
-        this._inputData.focus()
+        this._inputData.focus();
     };
 
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Lista apagada com sucesso';
+        this._mensagemView.update(this._mensagem);
+    };
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,14 +1,21 @@
 class ListaNegociacoes {
 
-    constructor(){
+    constructor(armadilha){
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
     }
 
     get negociacoes(){
         return [].concat(this._negociacoes)
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+        this._armadilha(this);
     }
 }
