f37c8ff0ef08b256dcbcf33eed5024b127fe73e7
Jeferson <36243087+devjef@users.noreply.github.com>
2020-06-24 22:27:51 -0300

    Aplicação de Factory e Data Binding
    Isolamento do código que constrói um Proxy e sua complexidade na classe ProxyFactory.
    
    ''Escondendo'' a construção do Proxy utilizando um objeto Bind.
    
    Alteração de NegociacaoController para usar as classes criadas.
    
    Remoção dos atributos que representam as views do controller passando direto para o Proxy, para apenas ele manipular a view!

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,46 +3,22 @@ class NegociacaoController {
     constructor() {
         let $ = document.querySelector.bind(document);
 
-        self = this;
-
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#alertaMensagem'));
-        this._mensagemView.update(this._mensagem);
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
 
-            get(target, prop, receiver) {
-                console.log(target)
-                console.log(prop)
-                console.log(receiver)
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')), 'adiciona', 'esvazia'
+        );
 
-                if (['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
-                    return function() {
-                        console.log(`Alteração usando o método ${prop} interceptada`);
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                        console.log(target)
-                    }
-                }
-
-                return Reflect.get(target, prop, receiver)
-            }
-
-        });
+        this._mensagem = new Bind(new Mensagem(), new MensagemView($('#alertaMensagem')), 'texto');
     };
 
     adiciona(e) {
+
         e.preventDefault();
-
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
-
         this._limpaInputs();
     };
 
@@ -63,10 +39,8 @@ class NegociacaoController {
     };
 
     apaga() {
-        this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
 
+        this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Lista apagada com sucesso';
-        this._mensagemView.update(this._mensagem);
     };
 }
