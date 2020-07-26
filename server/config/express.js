1100da37855af358a41f98cb30d2120da9bb3cda
Jeferson <36243087+devjef@users.noreply.github.com>
2020-07-26 18:54:54 -0300

    Salvando dados localmente com IndexedDB
    Armazenamento de negociações offline com IndexedDB
    Gerenciamento de conexão com o pattern Factory 
    Padronização do acesso aos dados com o pattern DAO 
    Simplificação das requisições Ajax com a Fetch API 
    Uso de Babel para tornar o código mais compatível
    Aplicação de módulos do ES2015!

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,10 @@
 /* Código simplório, apenas para fornecer o serviço para a aplicação */
 
-var express = require('express'),
-    app = express(),
-    routes = require('../app/routes'),
-    path = require('path'),
-    bodyParser = require('body-parser');
+var express = require('express')
+    ,app = express()
+    ,routes = require('../app/routes')
+    ,path =  require('path')
+    ,bodyParser = require('body-parser');
 
 app.set('clientPath', path.join(__dirname, '../..', 'client'));
 console.log(app.get('clientPath'));
@@ -12,9 +12,9 @@ app.use(express.static(app.get('clientPath')));
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
